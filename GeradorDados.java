644277643a773bbf388a858612652ea3e1673487
thiagograsso <143461239+thiagograsso@users.noreply.github.com>
2024-11-22 06:03:05 -0300

    Update GeradorDados.java

--- GeradorDados.java
+++ GeradorDados.java
@@ -3,12 +3,12 @@ import java.util.Random;
 public class GeradorDados {
     public static Registro[] gerarRegistros(int quantidade) {
         Registro[] registros = new Registro[quantidade];
-        Random random = new Random(123); // Seed fixa para consistência
+        Random random = new Random(123); 
 
         for (int i = 0; i < quantidade; i++) {
             StringBuilder codigo = new StringBuilder();
             for (int j = 0; j < 9; j++) {
-                codigo.append(random.nextInt(10)); // Gera um dígito de 0 a 9
+                codigo.append(random.nextInt(10)); 
             }
             registros[i] = new Registro(codigo.toString(), "Valor " + i);
         }
