63e8337865b954467bef24b32d9f4c0369c479c9
thiagograsso <143461239+thiagograsso@users.noreply.github.com>
2024-11-29 16:36:54 -0300

    Update FuncaoHash.java

--- FuncaoHash.java
+++ FuncaoHash.java
@@ -1,7 +1,20 @@
 public abstract class FuncaoHash {
+
+
+    public int tamanhoString(String chave) {
+        int tamanho = 0;
+        char[] caracteres = chave.toCharArray();
+        for (char c : caracteres) {
+            tamanho++;
+        }
+        return tamanho;
+    }
+
+
     public int hash(String chave, int tamanhoTabela) {
         int soma = 0;
-        for (int i = 0; i < chave.length(); i++) {
+        int tamanho = tamanhoString(chave);
+        for (int i = 0; i < tamanho; i++) {
             soma += chave.charAt(i);
         }
         return soma % tamanhoTabela;
