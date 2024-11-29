e35a33ddeee90e33c56aa030dd9e89a02cee2735
thiagograsso <143461239+thiagograsso@users.noreply.github.com>
2024-11-29 16:35:19 -0300

    Update HashDivisao.java

--- HashDivisao.java
+++ HashDivisao.java
@@ -2,8 +2,9 @@ public class HashDivisao extends FuncaoHash {
     @Override
     public int calcularHash(String codigo) {
         int hash = 0;
-        for (int i = 0; i < codigo.length(); i++) {
-            hash = (hash * 31 + codigo.charAt(i)) % 1000; 
+        int tamanho = tamanhoString(codigo);
+        for (int i = 0; i < tamanho; i++) {
+            hash = (hash * 31 + codigo.charAt(i)) % 1000;
         }
         return hash;
     }
