4e4ecb4c3fba837891a62633df1586fc284af45e
thiagograsso <143461239+thiagograsso@users.noreply.github.com>
2024-11-29 16:34:46 -0300

    Update HashDobramento.java

--- HashDobramento.java
+++ HashDobramento.java
@@ -2,7 +2,7 @@ public class HashDobramento extends FuncaoHash {
     @Override
     public int calcularHash(String codigo) {
         int hash = 0;
-        for (int i = 0; i < 9; i += 2) { 
+        for (int i = 0; i < 9; i += 2) {
             int parte = (codigo.charAt(i) - '0') * 10;
             if (i + 1 < 9) {
                 parte += (codigo.charAt(i + 1) - '0');
